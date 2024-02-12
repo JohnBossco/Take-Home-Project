968032f6a221e8e2e7f0ab6ffa45aa8802925e24
unknown <Johnmuiruri352@gmail.com>
2024-02-12 01:28:26 -0600

    disabled page to only allow after new date is selected

--- frontend/src/homePageComponents/Booking.js
+++ frontend/src/homePageComponents/Booking.js
@@ -1,101 +1,122 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { FaPlus } from "react-icons/fa";
 import { format } from "date-fns";
 
-
-const Booking = () =>{
-  
+const Booking = () => {
   const handleBook = (e) => {
     e.preventDefault();
     console.log(e.target.value);
-  }
+  };
 
 
-  let defaultDate = new Date().toISOString().split("T")[0]
-  console.log(defaultDate)
 
+  let defaultDate = new Date().toISOString().split("T")[0];
+  // console.log(defaultDate)
 
-  const [curDeptDate, setDeptDate] = useState(defaultDate)
-  console.log(curDeptDate, setDeptDate)
-  
+  let defaultFix = defaultDate.split("-").join(",");
+      defaultFix = format(new Date(defaultFix), "M/d/yyyy");
+  // console.log(defaultFix);
 
+  const [curDeptDate, setDeptDate] = useState(defaultDate);
+  // console.log(curDeptDate, setDeptDate)
 
-  let almostFix = curDeptDate.split("-").join(",")
-  console.log(almostFix)
-  
+  let almostFix = curDeptDate.split("-").join(",");
+  // console.log(almostFix)
 
+  let dateFix = format(new Date(almostFix), "M/d/yyyy");
+  // console.log(dateFix);
 
-  let dateFix = format(new Date(almostFix), "M/d/yyyy")
-  console.log(dateFix)
-
+  const isItSameDate =  dateFix === defaultFix
 
 
 
   return (
-  <div className='md:mt-[160px] mt-[50px] mx-4 relative'>
-    <div className='bg-white rounded-md max-w-6xl w-full mx-auto'>
-        <form className='flex flex-col md:flex-row'>
-        
-         {/* Name */}
-         <div className='py-1.5 px-2.5 flex-1 border-r-2'>
-            <p><strong>Enter Name</strong></p>
+    <div className="md:mt-[160px] mt-[50px] mx-4 relative">
+      <div className="bg-white rounded-md max-w-6xl w-full mx-auto">
+        <form className="flex flex-col md:flex-row">
+          {/* Name */}
+          <div className="py-1.5 px-2.5 flex-1 border-r-2">
+            <p>
+              <strong>Enter Name</strong>
+            </p>
             <div>
               <label htmlFor="name"></label>
-              <input type="text" id='name' required/>
+              <input type="text" id="name" required />
             </div>
           </div>
 
-          {/* Boarding date */}
-          <div className='py-1.5 px-2.5 flex-1 border-r-2'>
-            <p><strong>Departure Date</strong></p>
-            <input defaultValue={defaultDate} onChange={(e) => setDeptDate(e.target.value)} type='date' name='date' required className='outline-none p-2 w-full'/>
-          </div>          
-         
-         {/* from  */}
-          <div className='py-1.5 px-2.5 flex-1 border-r-2'>
-            <strong><p>Destination From</p></strong>
+          {/* Departure date */}
+          <div className="py-1.5 px-2.5 flex-1 border-r-2">
+            <p>
+              <strong>Departure Date</strong>
+            </p>
+            <input
+              onChange={(e) => setDeptDate(e.target.value)}
+              type="date"
+              name="date"
+              required
+              className="outline-none p-2 w-full"
+            />
+          </div>
+
+          {/* Destination from  */}
+          <div className="py-1.5 px-2.5 flex-1 border-r-2">
+            <strong>
+              <p>Destination From</p>
+            </strong>
             <div>
-              <select name='from' id='from' className='outline-none p-2 w-full'>
+              <select disabled={isItSameDate} name="from" id="from" className="outline-none p-2 w-full">
                 <option value="">Please Select</option>
-                <option value="" >Locations Americas A</option>
-                <option value="" >Locations Americas B</option>
-                <option value="" >Locations Europe A</option>
-                <option value="" >Locations Europe B</option>
+                <option value="">Locations Americas A</option>
+                <option value="">Locations Americas B</option>
+                <option value="">Locations Europe A</option>
+                <option value="">Locations Europe B</option>
               </select>
             </div>
           </div>
 
           {/* Arrival Date  */}
-          <div className='py-1.5 px-2.5 flex-1 border-r-2'>
-            <strong><p>Arrival Date</p></strong>
-            <input type='date' name='date' required className='outline-none p-2 w-full'/>
-          </div> 
+          <div className="py-1.5 px-2.5 flex-1 border-r-2">
+            <strong>
+              <p>Arrival Date</p>
+            </strong>
+            <input
+              type="date"
+              name="date"
+              required
+              className="outline-none p-2 w-full"
+            />
+          </div>
 
           {/* destination to */}
-          <div className='py-1.5 px-2.5 flex-1 border-r-2'>
-            <strong><p>Destination To</p></strong>
+          <div className="py-1.5 px-2.5 flex-1 border-r-2">
+            <strong>
+              <p>Destination To</p>
+            </strong>
             <div>
-              <select name='from' id='from' className='outline-none p-2 w-full'>
+              <select name="from" id="from" className="outline-none p-2 w-full">
                 <option value="">Please Select</option>
-                <option value="" >Locations Americas A</option>
-                <option value="" >Locations Americas B</option>
-                <option value="" >Locations Europe A</option>
-                <option value="" >Locations Europe B</option>
+                <option value="">Locations Americas A</option>
+                <option value="">Locations Americas B</option>
+                <option value="">Locations Europe A</option>
+                <option value="">Locations Europe B</option>
               </select>
-
             </div>
           </div>
 
-
-
           {/* Book Button */}
-          <button onClick={(e) => handleBook(e)} type='submit' className='bg-indigo-500 text-white px-8 py-1 space-x-2 text-c flex items-center justify-center'><FaPlus className='mr=1' />Book Now</button>
-
+          <button
+            onClick={(e) => handleBook(e)}
+            type="submit"
+            className="bg-indigo-500 text-white px-8 py-1 space-x-2 text-c flex items-center justify-center"
+          >
+            <FaPlus className="mr=1" />
+            Book Now
+          </button>
         </form>
       </div>
     </div>
-  )
-  
-  }
+  );
+};
 
-export default Booking
+export default Booking;
